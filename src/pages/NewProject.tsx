6ace7468e9b52de61b46249891bd8dc396b07d70
shira511 <ttppkk0511@gmail.com>
2025-05-12 21:42:10 +0900

    add error log top

--- src/pages/NewProject.tsx
+++ src/pages/NewProject.tsx
@@ -1,3 +1,4 @@
+console.log("🟢 NewProject component rendered");
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
